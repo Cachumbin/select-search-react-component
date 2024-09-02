2a70fe9ad2f4ea6826f7e208ac715a8c864d6347
Cachumbin <simoncarreno2006@gmail.com>
2024-09-02 17:27:25 -0500

    style: Fixed a div structure to display the styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 import Select2 from './Components/Select';
 
 const App: React.FC = () => {
 
   return (
-    <Select2/>
+    <div><Select2/></div>
   );
 }
 
